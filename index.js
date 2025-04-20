05033ed998d9172e436d5789cf6e23bcdb909c7c
Daniela Pech <randakova.daniela@gmail.com>
2025-04-20 20:23:50 +0200

    vypracovane cviceni, fce jako argumenty

--- index.js
+++ index.js
@@ -6,9 +6,28 @@ const fillSubject = (subject) => {
   document.querySelector('.email__subject').textContent = subject;
 };
 
-const fillBody = (body, name) => {
+const fillBody = (body, name, goodbyeFunction) => {
+  const greetingElm = document.querySelector('.email__greeting');
+  greetingElm.innerHTML = `Vážený pane ${name}`;
   const bodyElm = document.querySelector('.email__body');
   bodyElm.innerHTML = body;
   const closingElm = document.querySelector('.email__closing');
-  closingElm.textContent = goodbye(name);
+  closingElm.innerHTML = goodbyeFunction;
 };
+
+const formalGoodbye = (name) => {
+  return 'S úctivou poklonou ' + name;
+};
+
+const rudeGoodbye = (name) => {
+  return `Se měj ${name}`;
+};
+
+fillSubject('Nový mop');
+
+fillSubject('Pozvánka na oslavu narozenin');
+fillBody(
+  'Zítra oslava. 18:00 ve Starý hospodě.',
+  'Patrik Veselý',
+  rudeGoodbye('Josef Veselý'),
+);
